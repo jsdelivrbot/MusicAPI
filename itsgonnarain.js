328a86444e331093e050392b140a4684d5e11f01
Brenden Seidel <brenseid@gmail.com>
2017-10-02 17:31:20 -0400

    working now

--- itsgonnarain.js
+++ itsgonnarain.js
@@ -1,15 +1,30 @@
 console.log("it's gonna rain!")
 
+let audioContext = new AudioContext();
 
-// let audioContext = new AudioContext();
+function startLoop(audioBuffer, pan = 0, rate = 1) {
+  let sourceNode = audioContext.createBufferSource();
+  let pannerNode = audioContext.createStereoPanner();
 
-// fetch('itsgonnarain.mp3')
-//   .then(response => response.arrayBuffer())
-//   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
-//   .then(audioBuffer => {
-//     let sourceNode = audioContext.createBufferSource();
-//     sourceNode.buffer = audioBuffer;
-//     sourceNode.connect(audioContext.destination);
-//     sourceNode.start();
-//   })
-//   .catch(e => console.error(e));
+  sourceNode.buffer = audioBuffer;
+  sourceNode.loop = true;
+  sourceNode.loopStart = 3.25;
+  sourceNode.loopEnd = 5.5;
+  sourceNode.playbackRate.value = rate;
+  pannerNode.pan.value = pan;
+
+  sourceNode.connect(pannerNode);
+  pannerNode.connect(audioContext.destination);
+
+  sourceNode.start(0);
+}
+
+fetch('itsgonnarain.mp3')
+  .then(response => response.arrayBuffer())
+  .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+  .then(audioBuffer => {
+    startLoop(audioBuffer, -1);
+    startLoop(audioBuffer, 1, 1.002);
+  })
+  .catch(e => console.error(e));
+
