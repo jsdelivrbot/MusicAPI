c3ec296b4c1709a57694178cd94c5d148b6d916d
Brenden Seidel <brendenseidel@Brendens-MacBook-Air.local>
2017-10-02 17:00:15 -0400

    First Commit

---
+++ itsgonnarain.js
@@ -0,0 +1,15 @@
+console.log("it's gonna rain!")
+
+
+// let audioContext = new AudioContext();
+
+// fetch('itsgonnarain.mp3')
+//   .then(response => response.arrayBuffer())
+//   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+//   .then(audioBuffer => {
+//     let sourceNode = audioContext.createBufferSource();
+//     sourceNode.buffer = audioBuffer;
+//     sourceNode.connect(audioContext.destination);
+//     sourceNode.start();
+//   })
+//   .catch(e => console.error(e));
